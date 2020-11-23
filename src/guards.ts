8c1b48b345b02b6e512c3e9d67d155afd72cdf08
Tim Geerts <tim.geerts1@gmail.com>
2020-11-23 14:55:44 +0100

    fix(key): fix issue where bot replied more than once (#18)

--- src/guards.ts
+++ src/guards.ts
@@ -1,27 +1,22 @@
-import { Client, ArgsOf, GuardFunction } from "@typeit/discord";
-import { Utils } from "./utils";
+import { Client, ArgsOf, GuardFunction } from '@typeit/discord';
+import { Utils } from './utils';
 
-export const NotBot: GuardFunction<"message"> = async (
-  [message],
-  client,
-  next
-) => {
+export const NotBot: GuardFunction<'message'> = async ([message], client, next) => {
   if (!message.author.bot) {
     await next();
   }
 };
 
-export const NotHandled: GuardFunction<"message"> = async (
-  [message],
-  client,
-  next
-) => {
+export const NotHandled: GuardFunction<'message'> = async ([message], client, next) => {
   const content = Utils.stripPrefix(message.content);
-  const cmds = Client.getCommands();
+  const actualCommand = content.split(' ')[0];
+  const existingCommands = Client.getCommands();
 
-  const found = cmds.some((c) => {
+  const found = existingCommands.some((c) => {
     const command = c.commandName.toString().toLocaleLowerCase();
-    return command.startsWith(content.toLocaleLowerCase());
+    console.log('existing command: ', command);
+    console.log('message: ', content);
+    return command.startsWith(actualCommand?.toLocaleLowerCase());
   });
   if (!found) {
     await next();
@@ -29,7 +24,7 @@ export const NotHandled: GuardFunction<"message"> = async (
 };
 
 export function HasPrefix(text: string) {
-  const guard: GuardFunction<"message"> = async ([message], client, next) => {
+  const guard: GuardFunction<'message'> = async ([message], client, next) => {
     const startWith = message.content.startsWith(text);
     if (startWith) {
       await next();
