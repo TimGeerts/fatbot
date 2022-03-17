952502dfb0fe0e72a4a67a452bc3f0595e3b8c2d
Tim Geerts <tim.geerts1@gmail.com>
2022-03-17 09:29:34 +0100

    feat(key): add option to add a remark to a key

--- src/modules/key.ts
+++ src/modules/key.ts
@@ -7,21 +7,16 @@ import { Utils } from '../utils';
 export abstract class Key {
   private client: Client;
 
-  @Command('wnl :key :level :tank :heal :dps')
-  @Infos({ description: "Displays a template for people to 'sign up' for a given 'weekly no leavers' key" })
-  async wnl(command: CommandMessage, client: Client) {
-    this.key(command, client, true);
-  }
-
-  @Command('keys :key :level :tank :heal :dps')
+  @Command('keys :key :level :tank :heal :dps :remark')
   @Infos({ description: "Displays a template for people to 'sign up' for a given key", forAdmins: true })
   async keys(command: CommandMessage, client: Client) {
     this.key(command, client);
   }
 
-  @Command('key :key :level :tank :heal :dps')
+  @Command('key :key :level :tank :heal :dps :remark')
   @Description("Displays a template for people to 'sign up' for a given key")
-  async key(command: CommandMessage, client: Client, wnl = false) {
+  async key(command: CommandMessage, client: Client) {
+    this.parseCommand(command);
     this.client = client;
     getDungeons()
       .then((dungeons: IDungeon[]) => {
@@ -32,14 +27,19 @@ export abstract class Key {
             helpEmbed
               .setColor('#007bff')
               .setTitle('Usage')
-              .setDescription('Some example usages of the `?key` (or `?wnl`) command')
+              .setDescription('Some example usages of the `?key` command')
               .addField(
                 'Syntax',
                 '`?key <dungeon> <level> <tank> <healer> <dps>`\n*(tank/healer/dps are optional parameters)*'
               )
+              .addField('Remark', 'If you want to add a specific remark to your key, add `remark: this is my remark`')
               .addField('Looking for a full group', '`?key MoTS 18`')
               .addField('Looking for two dps', '`?key HoA 18 0 0 2`')
-              .addField('Looking for tank and healer', '`?key PF 18 1 1 0`');
+              .addField('Looking for tank and healer', '`?key PF 18 1 1 0`')
+              .addField(
+                'Looking for one dps (with remark)',
+                '`?key PF 18 0 0 1 remark: preferably a dps with bloodlust`'
+              );
             const dungeon_acronyms = dungeons
               .sort((a, b) => (a.name > b.name ? 1 : -1))
               .map((d) => `${d.name}: \`${d.tags[0]}\``);
@@ -49,18 +49,18 @@ export abstract class Key {
           } else {
             const key = command.args.key;
             const level = command.args.level;
+            if (!level || isNaN(level)) {
+              throw new Error(`No keylevel could be determined from the parameter \`${level}\``);
+            }
             const missingRoles = this.findMissingRoles(command.args);
             const dungeon = dungeons.find((d) => d.tags.map((t) => t.toLowerCase()).indexOf(key?.toLowerCase()) > -1);
             if (!dungeon) {
               throw new Error(`No dungeon was found for the parameter  \`${key}\``);
             }
-            if (!level || isNaN(level)) {
-              throw new Error(`No keylevel could be determined from the parameter \`${level}\``);
-            }
             command.args.key = dungeon.name;
             const chan = command.channel;
             chan.send(Utils.getPingStringForRoles(missingRoles, command.guild)).then((pingMsg: Message) => {
-              keyEmbed = this.createEmbed(command, wnl);
+              keyEmbed = this.createEmbed(command);
               chan.send(keyEmbed).then((m: Message) => {
                 //change the author of the message to be the one that sent the command
                 m.author = command.author;
@@ -80,6 +80,30 @@ export abstract class Key {
       .catch((err: Error) => {
         command.reply(`Sorry, I had some trouble fetching that information.\n\n${err.message}`);
       });
+  }
+
+  private parseCommand(command: CommandMessage): void {
+    // find the remark (if any)
+    const rgxRemark = / remark:(.*)/g;
+    const rgxCmd = /\?keys|\?key/g;
+
+    // remove the ?key or ?keys part of the command
+    let content = command.content.replace(rgxCmd, '').trim();
+    // get the remark from the command (if any)
+    const remark = content.match(rgxRemark);
+
+    // a remark: pattern was found, store it as an argument and remove it from the command.content
+    if (remark?.length) {
+      let remarkArg = remark.shift();
+      const actualCommand = content.replace(rgxRemark, '').trimEnd().split(' ');
+      // set the actual args of the command
+      command.args.key = actualCommand[0];
+      command.args.level = +actualCommand[1];
+      command.args.tank = this.numberOrDefault(actualCommand[2], 1);
+      command.args.heal = this.numberOrDefault(actualCommand[3], 1);
+      command.args.dps = this.numberOrDefault(actualCommand[4], 3);
+      command.args.remark = remarkArg.replace('remark:', '').trim();
+    }
   }
 
   // returns an array of roles that are being looked for
@@ -136,18 +160,23 @@ export abstract class Key {
   }
 
   // create the initial embed based on the command parameters
-  private createEmbed(command: CommandMessage, wnl = false): MessageEmbed {
+  private createEmbed(command: CommandMessage): MessageEmbed {
     const key = command.args.key;
     const level = command.args.level;
     const tank = command.args.tank;
     const heal = command.args.heal;
+    const remark = command.args.remark;
     const title = `[LFG] ${key} +${level}`;
     let dps = command.args.dps;
     const embed = new MessageEmbed().setColor('#e6cc80');
+    embed.setTimestamp();
     embed.setTitle(title);
-    if (wnl) {
-      embed.setDescription(':bangbang: __**WEEKLY NO LEAVERS**__ :bangbang:');
+    if (remark) {
+      embed.setDescription('```' + remark + '```');
+    } else {
+      embed.setDescription(`*${this.motivationalDescription()}*`);
     }
+    embed.addField('\u200B', '\u200B', true);
     if (tank === 1 || tank === undefined) {
       embed.addField(Utils.getEmoji('Tank'), '...');
     }
@@ -202,5 +231,26 @@ export abstract class Key {
     ids.forEach((i) => channel.messages.delete(i));
     // channel.messages.delete(ids[0]);
     //  .fetchMessage(lastmsg).then(msg => msg.delete());
+  }
+
+  private numberOrDefault(val: string, def: number): number {
+    return isNaN(+val) ? def : +val;
+  }
+
+  private motivationalDescription(): string {
+    const motivations = [
+      "I'm sure it will go well...",
+      "Remember, don't stand in bad shit!",
+      'Who pulled that!!!',
+      'God damnit, why did we get Wo relic...',
+      'Skip that shit',
+      'What are mechanics?',
+      'Leeeeeeeeerooooooooy',
+      'Just ... one ... more ...',
+      'Do you neeeeed?',
+      'Oops, wrong talents',
+      'Oops, wrong gearset...',
+    ];
+    return motivations[Math.floor(Math.random() * motivations.length)];
   }
 }
